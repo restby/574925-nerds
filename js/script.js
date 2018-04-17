71a5ec14fb36d7a3e8b266bb283a6b313a1f12f5
Anton <restby@mail.ru>
2018-04-17 20:56:42 +0300

    17.04.18H

--- js/script.js
+++ js/script.js
@@ -4,6 +4,42 @@ var modalWindowClose = document.querySelector('.modal-close');
 var nameFocus = document.querySelector('[name=name]');
 var staticMap = document.querySelector('.static-map');
 var wrapperMap = document.querySelector('.map');
+var slider0 = document.querySelector('.slider-item:nth-child(1)');
+var slider1 = document.querySelector('.slider-item:nth-child(2)');
+var slider2 = document.querySelector('.slider-item:nth-child(3)');
+var sliderControls0 = document.querySelector('.slider-controls i:nth-child(1)');
+var sliderControls1 = document.querySelector('.slider-controls i:nth-child(2)');
+var sliderControls2 = document.querySelector('.slider-controls i:nth-child(3)');
+if(sliderControls0 && sliderControls1 && sliderControls2) {
+    sliderControls0.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    slider1.classList.remove('slider-item-show');
+    slider2.classList.remove('slider-item-show');
+    slider0.classList.add('slider-item-show');
+    sliderControls0.classList.add('active');
+    sliderControls1.classList.remove('active');
+    sliderControls2.classList.remove('active');
+  });
+  sliderControls1.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    slider2.classList.remove('slider-item-show');
+    slider0.classList.remove('slider-item-show');
+    slider1.classList.add('slider-item-show');
+    sliderControls0.classList.remove('active');
+    sliderControls2.classList.remove('active');
+    sliderControls1.classList.add('active');
+  });
+  sliderControls2.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    slider0.classList.remove('slider-item-show');
+    slider1.classList.remove('slider-item-show');
+    slider2.classList.add('slider-item-show');
+    sliderControls0.classList.remove('active');
+    sliderControls1.classList.remove('active');
+    sliderControls2.classList.add('active');
+  });
+}
+
 openModal.addEventListener('click', function(evt) {
   evt.preventDefault();
   modalWindow.classList.add('modal-write-open');
